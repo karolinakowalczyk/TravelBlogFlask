5dca013bf44b6926afd866e5c421b889847216cb
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2023-07-06 02:32:16 +0200

    test

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from views import site
 
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+#app.secret_key = 'secret'
 
-app.register_blueprint(site, url_prefix="")
+#app.register_blueprint(site, url_prefix="")
+
+@app.route('/')
+def hello():
+    return "Flask heroku app"
 
 
 if __name__ == "__main__":
     app.run()
-# @app.route('/')
-# def hello():
-#     return "Flask heroku app"
